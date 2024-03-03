f4c3f26c5d4753668196bcc55a974e386a887329
Nazım Can Altınova <canaltinova@gmail.com>
2024-03-04 00:57:26 +0100

    Use Condvar instead of a raw futex
    Condvar uses futex internally.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use libc::{c_void, syscall};
 use once_cell::sync::Lazy;
 use std::sync::atomic::{AtomicI32, AtomicU64, Ordering};
-use std::sync::Arc;
-use std::{mem, ptr, thread, time};
+use std::sync::{Arc, Condvar, Mutex};
+use std::{mem, thread, time};
 
-static FUTEX_VALUE: Lazy<Arc<AtomicI32>> = Lazy::new(|| Arc::new(AtomicI32::new(0)));
+static SHARED_VALUE: Lazy<Arc<AtomicI32>> = Lazy::new(|| Arc::new(AtomicI32::new(0)));
 
 unsafe extern "C" fn sigprof_handler(
     _: libc::c_int,
@@ -12,28 +11,78 @@ unsafe extern "C" fn sigprof_handler(
     _: *mut libc::c_void,
 ) {
     // Modify the shared data
-    let new_value = 1;
-    FUTEX_VALUE.store(new_value, Ordering::SeqCst);
-
-    // Wake up the waiting thread
-    let futex_addr = FUTEX_VALUE.as_ptr() as *mut libc::c_void;
-    unsafe {
-        syscall(
-            libc::SYS_futex,
-            futex_addr,
-            libc::FUTEX_WAKE,
-            new_value,
-            ptr::null::<c_void>(),
-            ptr::null::<c_void>(),
-            0,
-        );
-    }
+    SHARED_VALUE.store(1, Ordering::SeqCst);
 }
 
 fn main() {
     let main_tid = unsafe { libc::gettid() };
     println!("[tid={}] starting", main_tid);
+
     // Setting up the signal handler.
+    setup_signal_handler(main_tid);
+
+    // Condvar value will be used to block the thread until the signal is received.
+    let condvar = Arc::new((Mutex::new(false), Condvar::new()));
+    let condvar_clone = Arc::clone(&condvar);
+    // We need to know about the pthread that will be waiting on the condvar.
+    let pthread = Arc::new(AtomicU64::new(0));
+    let pthread_clone = Arc::clone(&pthread);
+
+    // Spawn a thread to wait on the futex
+    let handle = std::thread::spawn(move || {
+        let tid = unsafe { libc::gettid() };
+        let pthread_self = unsafe { libc::pthread_self() };
+        pthread_clone.store(pthread_self, Ordering::SeqCst);
+
+        println!("[tid={}] waiting for condvar now", tid);
+
+        let (mutex, cvar) = &*condvar_clone;
+        let mut done = mutex.lock().unwrap();
+        while !*done {
+            done = cvar.wait(done).unwrap();
+        }
+
+        println!("[tid={}] awakened!", tid);
+    });
+
+    // First we need to know the pthread that will be waiting on the condvar.
+    while pthread.load(Ordering::SeqCst) == 0 {}
+
+    let pthread = pthread.load(Ordering::SeqCst);
+    println!("[tid={}] got the pthread {}", main_tid, pthread);
+
+    // Wait a bit just to make sure it's waiting for futex now.
+    thread::sleep(time::Duration::from_millis(100));
+
+    // Send the signal to the waiting thread.
+    let res = unsafe { libc::pthread_kill(pthread, libc::SIGPROF as libc::c_int) };
+    println!("[tid={}] sent the signal", main_tid);
+    assert!(res == 0);
+
+    println!(
+        "[tid={}] waiting for the shared value to change from the signal handler...",
+        main_tid
+    );
+    while SHARED_VALUE.load(Ordering::SeqCst) == 0 {}
+
+    println!("[tid={}] shared value has changed!", main_tid);
+    // We notify the condvar that the value has changed.
+    {
+        let (lock, cvar) = &*condvar;
+        let mut done = lock.lock().unwrap();
+        *done = true;
+        cvar.notify_one();
+    }
+
+    println!("[tid={}] notified the condvar", main_tid);
+
+    // Wait for the thread to finish.
+    handle.join().unwrap();
+
+    println!("[tid={}] test case finished as expected!", main_tid);
+}
+
+fn setup_signal_handler(main_tid: i32) {
     let mut s = mem::MaybeUninit::<libc::sigaction>::uninit();
     let sig_action = unsafe {
         let p = s.as_mut_ptr();
@@ -58,50 +107,4 @@ fn main() {
     };
     assert!(res == 0);
     println!("[tid={}] signal is set up", main_tid);
-
-    let futex_value_clone = Arc::clone(&FUTEX_VALUE);
-
-    let pthread = Arc::new(AtomicU64::new(0));
-    let pthread_clone = Arc::clone(&pthread);
-
-    // Spawn a thread to wait on the futex
-    let handle = std::thread::spawn(move || {
-        let tid = unsafe { libc::gettid() };
-        let pthread_self = unsafe { libc::pthread_self() };
-        pthread_clone.store(pthread_self, Ordering::SeqCst);
-        println!("[tid={}] waiting for futex", tid);
-        let futex_addr = futex_value_clone.as_ptr() as *mut libc::c_void;
-        unsafe {
-            syscall(
-                libc::SYS_futex,
-                futex_addr,
-                libc::FUTEX_WAIT,
-                0,
-                ptr::null::<c_void>(),
-                ptr::null::<c_void>(),
-                0,
-            )
-        };
-        println!("[tid={}] awakened!", tid);
-    });
-
-    while pthread.load(Ordering::SeqCst) == 0 {}
-    println!(
-        "[tid={}] got the pthread {}",
-        main_tid,
-        pthread.load(Ordering::SeqCst)
-    );
-
-    // Wait a bit just to make sure it's waiting for futex now.
-    thread::sleep(time::Duration::from_millis(100));
-
-    let res =
-        unsafe { libc::pthread_kill(pthread.load(Ordering::SeqCst), libc::SIGPROF as libc::c_int) };
-    println!("[tid={}] sent the signal", main_tid);
-    assert!(res == 0);
-
-    // Wait for the thread to finish.
-    handle.join().unwrap();
-
-    println!("[tid={}] test case finished as expected!", main_tid);
 }
